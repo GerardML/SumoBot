5ae3b1d308c038ee6a7e7fa692650c337677f190
GerardML <menesesgerard@gmail.com>
2021-02-23 20:49:57 -0600

    PWM On con CleanCode

--- Programa Sumobot.c
+++ Programa Sumobot.c
@@ -33,8 +33,11 @@
 //ASIGNACIÓN DE PINES  DE SALIDA: RD0 -> RD3
 
 ///////////Asignación de etiquetas/nombres a los comandos Hexa//////////////////   
-int adelante = 0x0F; //Se le asigna a "adelante" el codigo Hexa
-int atras = 0x00;
+int avanzar     = 0b1001; //Se le asigna a "adelante" el codigo Hexa
+int reversa     = 0b0110;
+int izquierda   = 0b1010;
+int derecha     = 0b0101;
+int detener     = 0b0000;
 
 
 int trig[5] = {trigIz, trigDe, trigCe, trigIzD, trigDeD};
@@ -46,7 +49,7 @@ float DCentro; //Equivale a la distancia
 void UltraSensor() 
 {
     //////////////SENSADO DE LOS ULTRASONICOS/////////////////
-    for (a = 0; a <= 4; a++) {
+    for (a = 0; a <= 4; a++) {  //Sensado de 5 UltraSonic
 
         float T, D;
         high(trig[a]);
@@ -62,7 +65,7 @@ void UltraSensor()
         if (a == 2) {
             DCentro = (T / 2) / (29.15);
         }
-        if (D < 45) {
+        if (D < 45) {    //Distancia de detección
             Sense[a] = 1;
         } else {
             Sense[a] = 0;
@@ -73,7 +76,7 @@ void UltraSensor()
     if (Sense[1] == 1) direc = 6; //#6 ya que el 2 daba problemas
     if (Sense[3] == 1) direc = 4;
     if (Sense[4] == 1) direc = 5;
-    if (Sense[2] == 1) direc = 3; //US Centro/Frontal
+    if (Sense[2] == 1) direc = 3; //UltraSonic Centro/Frontal
 
 }
 
@@ -82,45 +85,44 @@ void Direcciones()
 {
 //////////////CAMBIO DE DIRECCIONES Y PWM EN ULTRASONICO FRONTAL////////////////
     switch (direc) {
-        case 2:
+        case 2: //detenido
         { //Case de Inicio, Se cambió de ->0 a ->2, ya que siempre comenzaba en el 2
-            llantas = atras;
+            llantas = reversa;
         }
             break;
-        case 1:
+        case 1: //Izquierda
         {
-            llantas = atras;
+            llantas = izquierda;
         }
             break;
-        case 6:
+        case 6: //Derecha
         {
-            llantas = adelante;
+            llantas = derecha;
         }
             break;
-        case 3:
+        case 3: //Centro Frontal
         {
 
             int a;
-            for (a = 8; a <= 44; a++) {
+            for (a = 8; a <= 44; a++) {   //Rango de variación PWM(cm)
                 if (DCentro > a) {
                     set_timer0(21);
-                    while (get_timer0() >= 21) //Para un parpadeo de 10ms con un postcaler de 128
-                    {
-                    }
-                    llantas = adelante;
-                } else llantas = atras;
+                    while (get_timer0() >= 21) //Para un parpadeo de 10ms con un prescaler de 128
+                    {}
+                    llantas = avanzar;
+                } else llantas = detener;
             }
 
         }
             break;
         case 4:
         {
-            llantas = adelante;
+            llantas = izquierda;
         }
             break;
         case 5:
         {
-            llantas = adelante;
+            llantas = derecha;
         }
             break;
     }
@@ -133,6 +135,7 @@ void main() {
     llantas = 0x00;
     setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
     setup_timer_0(RTCC_INTERNAL | RTCC_DIV_128);
+    delay_ms(3000);
 
     for (;;) {
         UltraSensor();
